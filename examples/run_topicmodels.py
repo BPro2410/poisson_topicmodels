83da1b363eef93f058eb084ebe001010300c9346
Bettina Gruen <bettina.gruen@wu.ac.at>
2026-06-22 13:16:31 +0200

    use consistent relative path for files in folder data

--- examples/run_topicmodels.py
+++ examples/run_topicmodels.py
@@ -173,11 +173,11 @@ estimated_params = tm4.train_step(num_steps=1000, lr=0.01)
 
 # -- Create embeddings --
 # embeds = create_word2vec_embedding_from_dataset(list(df1["Text"]))
-# save_embeds(embeds, "data/embeds.bin")
+# save_embeds(embeds, "../data/embeds.bin")
 
 # -- Load embeddings --
 # path to embeddings
-path_to_embeddings = "data/embeds.bin"
+path_to_embeddings = "../data/embeds.bin"
 embeddings_mapping = load_embeds(path_to_embeddings)
 
 # -- RUN ETM --
