5a665ee0138cf95664c92905e1add24b4e12f0b2
Bernd Prostmaier <b.prostmaier@icloud.com>
2026-08-03 18:55:32 +0200

    fix: fix test import

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -8,7 +8,7 @@ def test_package_imports():
     import poisson_topicmodels
 
     assert hasattr(poisson_topicmodels, "__version__")
-    assert poisson_topicmodels.__version__ == "0.2.2"
+    assert poisson_topicmodels.__version__ == "1.0.0"
 
 
 def test_package_metadata():
